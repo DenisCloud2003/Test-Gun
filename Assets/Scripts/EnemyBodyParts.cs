3c8ad101e6487fe79bd21d2609a23d6f3560974e
Nguyen Duc Duy <jhvgame123@gmail.com>
2023-07-27 17:41:43 +0700

    first

---
+++ Assets/Scripts/EnemyBodyParts.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBodyParts : MonoBehaviour {
+    public enum ColliderBodyType { head, body, arms, legs}
+    public ColliderBodyType type;
+
+}
