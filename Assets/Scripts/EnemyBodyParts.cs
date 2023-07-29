fba8f256ea5d26f43433b7c960acc521bf3c282c
Nguyen Duc Duy <jhvgame123@gmail.com>
2023-07-29 23:17:17 +0700

    reset

--- Assets/Scripts/EnemyBodyParts.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class EnemyBodyParts : MonoBehaviour {
-    public enum ColliderBodyType { head, body, arms, legs}
-    public ColliderBodyType type;
-
-}
